c827f40d42d32071d0b4ae4faad0b1d711ba0797
samjain233 <94921996+samjain233@users.noreply.github.com>
2025-02-01 17:17:11 +0530

    feat/create-fetchfaq-controller

--- controller/faq.controller.js
+++ controller/faq.controller.js
@@ -1,14 +1,36 @@
 import FAQ from "../models/faq.model";
 import translateText from "../utils/translate.js";
 
-export const fetchFaqController = async (req, res) => {};
+export const fetchFaqController = async (req, res) => {
+  const { lang } = req.query;
+
+  try {
+    const faqs = await FAQ.find(); // Fetch all FAQs
+
+    const formattedFaqs = faqs.map((faq) => {
+      if (lang && faq.translations[lang]) {
+        return {
+          question: faq.translations[lang].question || faq.question, // Fallback to English
+          answer: faq.translations[lang].answer || faq.answer,
+        };
+      }
+      return faq; // if lang is not present then return the english faq
+    });
+
+    res.status(200).json(formattedFaqs);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching FAQs", error });
+  }
+};
 
 export const createFaqController = async (req, res) => {
   const { question, answer } = req.body;
 
   // Validation: Ensure fields are present
   if (!question || !answer) {
-    return res.status(400).json({ message: "Both question and answer are required." });
+    return res
+      .status(400)
+      .json({ message: "Both question and answer are required." });
   }
 
   try {
